bb55329bca23c8372a31dfc8b150b783f98fabd9
李有鬼 <158174692@qq.com>
2017-10-12 15:19:57 +0800

    li

--- TongBao/js/config.js
+++ TongBao/js/config.js
@@ -1,1 +1,21 @@
 var ip = 'http://192.168.1.120:8080';
+
+function post(url, data, callback, callback1) {
+	$.ajax({
+		type: "post",
+		url: ip + url,
+		async: true,
+		data: data,
+		success: function(data) {
+			callback(data);
+		},
+		error: function() {
+			new Vue().$dialog.alert({
+				mes: '服务器连接失败!'
+			});
+			if(callback1 != null) {
+				callback1();
+			}
+		}
+	});
+}
